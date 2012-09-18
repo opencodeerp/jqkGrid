66a1a39f443713988e2726bc686a69e065482595
sizers <sizers@korea.com>
2012-09-18 23:29:13 +0900

    작업한거

---
+++ WebContent/gridwork/js/jqkGridCommInclude.js
@@ -0,0 +1,24 @@
+
+
+document.write ('  \
+	<!--style-->\
+	<link rel="stylesheet" type="text/css" media="screen" href="css/jquiThemes/sunny/jquery-ui-1.8.21.custom.css" />			\
+	<link rel="stylesheet" type="text/css" media="screen" href="css/ui.jqkGrid.css" />			\
+	<!--jQuery, jQueryUI-->\
+	<script src="js/jquery-1.7.2.js" type="text/javascript"></script>			\
+	<script src="js/jquery-ui-1.8.21.custom.js" type="text/javascript"></script>			\
+	<script src="js/jqGridI18n/grid.locale-en.js" type="text/javascript"></script>			\
+	<script type="text/javascript"> 			\
+		//$.jgrid.no_legacy_api = true;			\
+		//$.jgrid.useJSON = true;			\
+	</script>			\
+	<!--jLinq--> \
+	<script src="js/jlinq.js" type="text/javascript"></script>			\
+	<!--jqGrid관련, jqkGrid-->\
+	<script src="js/jquery.jqGrid2.src.js" type="text/javascript"></script>			\
+	<script src="js/jquery.jqkGrid.src.js" type="text/javascript"></script>			\
+	<script src="js/jquery.tablednd.js" type="text/javascript"></script>			\
+	<!--요것들은 추후에 분석해 보자.. 일단은 모르것다..-->			\
+	<script src="js/jquery.contextmenu.js" type="text/javascript"></script>			\
+	<script src="js/ui.multiselect.js" type="text/javascript"></script>			\
+');
